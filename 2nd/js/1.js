c8c52c5fdb2a470f486512c74abf037be45b8c38
unknown <coder.srit@gmail.com>
2025-08-14 18:49:31 +0900

    Added smth but 3rd is broken

--- 2nd/js/1.js
+++ 2nd/js/1.js
@@ -1,3 +1,10 @@
-for (let temp = 1; temp <= 14; temp++) {
-    document.write("<tr><td>" + temp + "</td><td> </td></tr>");
+function time_lol() {
+    let now = new Date();
+    let display = now.toLocaleDateString();
+    document.getElementById("clock").innerHTML = "Current Time is " + display;
 }
+setInterval(time_lol, 1000);
+let num = 1; //value inserted as interger
+let num2 = "2"; //value inserted as text
+console.log(typeof num);
+console.log(typeof num2);
